7cbb473215868751528990833bd449cec43d3e51
Ivan-Prystay <prystay.ivan@gmail.com>
2023-01-16 11:07:12 +0200

    11

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+// import { rootReducer } from "./reducer";
+import { taskReducer, filtersReducer } from "./reducer";
+
+export const store = configureStore({
+  reducer: {
+    tasks: taskReducer,
+    filters: filtersReducer,
+  },
+});
